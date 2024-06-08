bcd09d1bec46d120cf8d97edb82bdc6838b7925d
akashsonune <akashsonune7720@gmail.com>
2024-06-08 13:46:50 +0530

    updates

--- Day-1-05/index.js
+++ Day-1-05/index.js
@@ -11,8 +11,8 @@ import AllRoutes from "./routes/index.js"
 const app = express();
 var corsOptions = {
   origin : [
-    // "http://localhost:3000",
-   "https://react-02-4l1e.vercel.app",
+    "http://localhost:3000",
+  //  "https://react-02-4l1e.vercel.app",
 ],
  credentials:true,
  };
